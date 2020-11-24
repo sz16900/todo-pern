39bd36c742d0a2dc1fa0c11d7fe4b3d57dda5ab0
sz16900 <sz16900@my.bristol.ac.uk>
2020-11-24 11:22:17 -1000

    add delete button to todo

---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,65 @@
+import React, { Fragment, useEffect, useState } from 'react';
+
+const ListTodos = () => {
+  // State
+  const [todos, setTodos] = useState([]);
+
+  //   Functions
+  const getTodos = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/todos');
+      const jsonData = await response.json();
+      setTodos(jsonData);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: 'DELETE',
+      });
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  // use effect is going to make a fetch request to a restful api everytime this component is rendered
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <Fragment>
+      <table className="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo) => (
+            <tr key={todo.todo_id}>
+              <td>{todo.description}</td>
+              <td>Edit</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListTodos;
